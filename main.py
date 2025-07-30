15b0fdf411a637f14322380806a88d3088503783
bharatvijayvargiya <bharat.vijayvargiya@gmail.com>
2025-07-30 20:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -352,13 +352,13 @@ def extract_minor_major_breaks(text: str):
             all_major_breaks.append(major_breaks)
 
         def deduplicate(breaks):
-        seen = set()
-        unique = []
-        for sent in breaks:
-            for item in sent:
-                if item[0] not in seen:
-                    unique.append(item)
-                    seen.add(item[0])
+            seen = set()
+            unique = []
+            for sent in breaks:
+                for item in sent:
+                    if item[0] not in seen:
+                        unique.append(item)
+                        seen.add(item[0])
             return unique
 
         return {
